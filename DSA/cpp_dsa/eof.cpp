cdce29ac4aadcbdef95e0941aa1536cc76c64cd6
nishan1314 <nishandas655@gmail.com>
2025-03-12 20:56:07 +0600

    Initial Commit

---
+++ DSA/cpp_dsa/eof.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+
+    int a,b,c;
+
+    while(cin>>a>>b>>c){
+        cout<<a<<" "<<b<<" "<<c<<endl;
+    }
+    
+    return 0;
+
+}
